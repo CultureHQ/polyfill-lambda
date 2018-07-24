bef28974f0d5ed2895bea88768f2dfba4c614885
Kevin Deisz <kevin.deisz@gmail.com>
2018-07-24 11:25:55 -0400

    Initial commit

---
+++ handler.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const { getPolyfillString } = require("polyfill-service");
+
+const makePolyfill = uaString => getPolyfillString({
+  uaString,
+  minify: true,
+  features: { es6: {}, "default-3.6": {} },
+  unknown: "polyfill"
+}).then(polyfill => ({
+  statusCode: 200,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/javascript;charset=utf-8",
+    "Content-Length": polyfill.length
+    "Cache-Control": "immutable"
+  },
+  body: polyfill
+}));
+
+const handle = (event, context, callback) => (
+  makePolyfill(event.headers["user-agent"])
+    .then(response => callback(null, response))
+    .catch(callback);
+);
+
+module.exports = { handle };
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { getPolyfillString } = require("polyfill-service");
+
+const app = express();
+
+const makePolyfill = uaString => polyfill.getPolyfillString({
+  uaString,
+  minify,
+  features: { es6: {}, "default-3.6": {} },
+  unknown: "polyfill"
+}).then(polyfill => ({
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/javascript;charset=utf-8",
+    "Content-Length": polyfill.length
+  },
+  body: polyfill
+}));
+
+module.exports = makePolyfill;
+
+
+app.options("/", (request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.end();
+});
+
+app.get("/", (request, response, next) => {
+  console.log(`[${new Date().toUTCString()}] GET /`);
+
+  makePolyfill(request.headers["user-agent"]).then(({ headers, body }) => {
+    Object.keys(headers).forEach(header => {
+      response.setHeader(header, headers[header]);
+    });
+
+    response.send(body);
+  }).catch(next);
+});
+
+app.listen(8081, () => {
+  console.log("Listening on port 8081...");
+});
